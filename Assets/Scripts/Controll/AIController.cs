094e4305f7fef5d35f5d8d61bc46a79c49e3b5ee
IlyaPav <il.pavl.2002@gmail.com>
2023-07-12 08:56:31 +0300

    implement  enemy AI logic: following to player, return back on guard position, waypoint patrol

--- Assets/Scripts/Controll/AIController.cs
+++ Assets/Scripts/Controll/AIController.cs
@@ -1,42 +1,117 @@
 using RPG.Combat;
+using RPG.Control;
+using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Controll
 {
     public class AIController : MonoBehaviour
     {
-        [SerializeField] float _chaseDistance = 5f;
+        private const string _playerTag = "Player";
+
+        [SerializeField] private float _chaseDistance = 5f;
+        [SerializeField] private float _suspicionTime = 5f;
+        [SerializeField] private float _waypointDdwellTime = 4f;
+        [SerializeField] private PatrolPath _patrolPath;
+        [SerializeField] private float waypointToleranceDistance = 1f;
 
         private Fighter _fighter;
-        private string _playerTag = "Player";
+        private Mover _mover;
+        private Health _health;
+
         private GameObject _player;
+        private Vector3 _guardPosition;
+        private float _timeSinceLastSawPlayer = Mathf.Infinity;
+        private float _timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        private int currentWaypointIndex = 0;
 
         private void Start()
         {
             _fighter = GetComponent<Fighter>();
+            _mover = GetComponent<Mover>();
+            _health = GetComponent<Health>();
             _player = GameObject.FindGameObjectWithTag(_playerTag);
 
+            _guardPosition = transform.position;
         }
         private void Update()
         {
-            if (_player == null) { return; }
+            if (_player == null || _health.IsDead) { return; }
 
             if (IsPlayerInAttackRange() && _fighter.CanAttack(_player))
             {
-                _fighter.Attack(_player);
+                AttackBehaviour();
             }
-            else
+            else if (_timeSinceLastSawPlayer < _suspicionTime)
             {
-                _fighter.Cansel();
+                SuspicionBehaviour();
             }
+            else if (_timeSinceArrivedAtWaypoint >= _waypointDdwellTime)
+            {
+                PatrolBehaviour();
+            }
+            UpdateTimers();
 
+        }
+
+        private void UpdateTimers()
+        {
+            _timeSinceLastSawPlayer += Time.deltaTime;
+            _timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehaviour()
+        {
+            var nextPosition = _guardPosition;
+            if (_patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    UpdateWaypointIndex();
+                    _timeSinceArrivedAtWaypoint = 0;
+                }
+                nextPosition = GetCurrentWaypointPosition();
+            }
+            _mover.StartMoveAction(nextPosition);
+        }
+
+        private bool AtWaypoint()
+        {
+            return Vector3.SqrMagnitude(transform.position - GetCurrentWaypointPosition()) <= waypointToleranceDistance;
+        }
+
+        private void UpdateWaypointIndex()
+        {
+            currentWaypointIndex = _patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private Vector3 GetCurrentWaypointPosition()
+        {
+            return _patrolPath.GetWaypointPosition(currentWaypointIndex);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            GetComponent<ActionScheduler>().CancelCurrenAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            _timeSinceLastSawPlayer = 0;
+            _fighter.Attack(_player);
         }
 
         private bool IsPlayerInAttackRange()
         {
             var distanceTopLayer = Vector3.SqrMagnitude(transform.position - _player.transform.position);
-            Debug.Log(distanceTopLayer);
             return distanceTopLayer < (_chaseDistance * _chaseDistance);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, _chaseDistance);
         }
     }
 }
